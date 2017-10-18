9015bb6cc5ee4b01a178e414fa1c2fe3c5f7cb71
s203 <s203@AI241LAB0203M12.local>
2017-10-18 11:51:33 -0700

    updates

---
+++ volume.converter.py
@@ -0,0 +1,4 @@
+print('Lets convert your volume calculations from gallons, to liters.')
+print('Enter your total amount of gallons you which to convert.')
+gallons = float(input())
+print('Your total volume in liters = ' , str(gallons * 3.7841))
