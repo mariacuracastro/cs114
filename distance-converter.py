996e551aefa7fe0711e38edab3300c540b0fd49d
s203 <s203@AI241LAB0203M12.local>
2017-11-01 12:01:42 -0700

    updates

---
+++ distance-converter.py
@@ -0,0 +1,19 @@
+print ('Enter the distance in miles:')
+
+miles = input()
+
+num_kilometers = float(input())
+print ('Your distance in kilometers =' , str(miles * 1.60934))
+
+num_feet = float(input())
+print ('Your distance in feet =' , str(miles * 5280))
+
+num_meters = float(input())
+print ('Your distance in meters =' , str(miles * 1609.34))
+
+print ('Enter the units:')
+print ('Enter the target units:')
+
+# # 1 mile = 1.60934 kilometer
+# 1 mile = 5280 feet
+# 1 mile = 1609.34 meters
